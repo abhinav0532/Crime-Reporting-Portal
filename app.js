507cfa858896acaba79a24a8e9511451252d957f
Siddharth Singh <siddharthsingh330@yahoo.in>
2021-03-26 16:16:25 +0530

    Added the login and register options

--- app.js
+++ app.js
@@ -78,6 +78,10 @@ app.get("/login", function(req, res){
   res.render("login");
 });
 
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });

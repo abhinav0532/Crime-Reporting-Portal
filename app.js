fd9b18bba3c079fc74cb4f00f5784d61db0bced8
Siddharth Singh <siddharthsingh330@yahoo.in>
2021-03-26 15:54:37 +0530

    Changes to all the files

--- app.js
+++ app.js
@@ -74,6 +74,9 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+app.get("/login", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
